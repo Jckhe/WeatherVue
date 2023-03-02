8d51a99e20810fbcfac980b2714abff5fcf79300
jckhe <iamjckhe@gmail.com>
2023-03-02 15:07:20 -0800

    Building out weathercard

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -10,25 +10,45 @@ export default {
       type: String,
       required: true,
     },
+    weatherData: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    formattedTitle() {
+      return `${this.cityName}, ${this.stateName}`;
+    },
+    titleStyle() {
+      return {
+        color: "white",
+      };
+    },
   },
 };
 </script>
 
 <template>
-  <div class="weather-card-container">
-    <div class="city-name-container">
-      <h1 class="city-name-header">{{ cityName }}, {{ stateName }}</h1>
-    </div>
-  </div>
+  <a-card
+    :title="formattedTitle"
+    :headStyle="titleStyle"
+    class="weather-card-container"
+  >
+  </a-card>
 </template>
 
 <style>
 .weather-card-container {
   border: 1px solid red;
+  background-color: transparent !important;
   height: 49.5vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
+}
+
+.title {
+  color: white !important;
 }
 </style>
